72d1d3f925fc6186d9673def66e69be66fc64a50
Stepan Rybak <stepanrbak@gmail.com>
2019-03-29 22:47:21 -0700

    Express Router

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser=require('body-parser');
+const dishRouter=require('./routes/dishRouter');
+
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,7 +11,10 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use('/dishes', dishRouter);
 
+//////////////////////////////////////////////////////////////////////////////////////
+/*
 app.all('/dishes', (req, res, next)=>{
   res.statusCode=200;
   res.setHeader('Content-Type', 'text/plain');
@@ -51,7 +56,7 @@ app.put('/dishes/:dishId', (req,res,next) => {
 app.delete('/dishes/:dishId', (req,res,next) => {
   res.end('Deleting dish: '+ req.params.dishId);
 });
-
+*/
 app.use(express.static(__dirname+ '/public'));
 
 app.use((req, res, next) => {
