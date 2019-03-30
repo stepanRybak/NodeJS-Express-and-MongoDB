1ec040a512f447292a1aec30b7946098feef2d11
Stepan Rybak <stepanrbak@gmail.com>
2019-03-30 11:05:45 -0700

    Express Router for dishes, leaders and promotions

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser=require('body-parser');
 const dishRouter=require('./routes/dishRouter');
+const promoRouter= require('./routes/promoRouter');
+const leaderRouter= require('./routes/leaderRouter');
 
 
 const hostname = 'localhost';
@@ -12,6 +14,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 //////////////////////////////////////////////////////////////////////////////////////
 /*
